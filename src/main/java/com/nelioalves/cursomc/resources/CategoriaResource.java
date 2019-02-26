8c2df4632c1f7d601f3709632421339a7890591a
Jefferson Sabino <jeffersoncbmtt@gmail.com>
2019-02-26 10:56:40 -0300

    Criando operação de instanciação

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,5 @@
 package com.nelioalves.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +17,8 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 	
+	//{id} = é um apelido referente ao id
+	//response entity - encapsula varias coisa numa resposta http
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
